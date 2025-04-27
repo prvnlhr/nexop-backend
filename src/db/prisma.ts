b6cfb9520e00741b985cc9087d9050257a78bdae
Praveen Lohar <praveenlohar.in@gmail.com>
2025-04-27 23:27:56 +0530

    removed dist

--- src/db/prisma.ts
+++ src/db/prisma.ts
@@ -1,5 +1,8 @@
 import { PrismaClient } from "../generated/prisma/client.js";
+// const prisma = new PrismaClient();
 
+// export default prisma;
+
+// import { PrismaClient } from "@generated/prisma";
 const prisma = new PrismaClient();
-
 export default prisma;
